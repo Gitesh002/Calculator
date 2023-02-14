9e35d492360f45ffb898898478fb4d602df0757e
CrazyDave <adrieldavid81169@gmail.com>
2023-02-14 23:56:42 +0530

    added switch statements

--- Main.cpp
+++ Main.cpp
@@ -13,8 +13,55 @@ public:
 };
 //main function
 int main()
-{   
-    Calc b(2,10);
-    std::cout<<b.mul()<<std::endl;
-    std::cout<<b.add()<<std::endl;
+{   int state {};
+    int x{}, y{};
+    std::cout << "Choose an option\n";
+    std::cout << "1. Addition\n";
+    std::cout << "2. Multiplication\n";
+    std::cout << "3. Subtraction\n";
+    std::cout << "4. Division\n";
+    std::cin >> state;
+    switch (state)
+    {
+    case 1: {
+        std::cout << "Enter number with spaces : ";
+        std::cin >> x;
+        std::cin >> y;
+        Calc b(x,y);
+        std::cout << b.add();
+        break;
+    }
+
+    case 2: {
+        std::cout << "Enter number with spaces : ";
+        std::cin >> x;
+        std::cin >> y;
+        Calc b(x,y);
+        std::cout << b.mul();
+        break;
+    }
+
+    case 3: {
+        std::cout << "Enter number with spaces : ";
+        std::cin >> x;
+        std::cin >> y;
+        Calc b(x,y);
+        std::cout << b.sub();
+        break;
+    }
+
+    case 4: {
+        std::cout << "Enter number with spaces : ";
+        std::cin >> x;
+        std::cin >> y;
+        Calc b(x,y);
+        std::cout << b.div();
+        break;
+    }
+    
+    default: {
+        std::cout << "Invalid";
+        break;
+    }
+    }
 }
