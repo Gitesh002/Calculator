aa6b4bda0f8da1caa42c673f1e8c67a424c97aad
Gitesh <giteshanand02@gmail.com>
2023-02-14 21:36:25 +0530

    Ok

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,15 @@ class Calc
 private:
     int x,y;
 public:
+    Calc(int num1, int num2) :x(num1),y(num2){}
     int add() {return x + y;}
     int mul() {return x * y;}
     int sub() {return x - y;}
     int div() {return x / y;}
 };
 int main()
-{
-
-    std::cin.get();
+{   
+    Calc b(2,10);
+    std::cout<<b.mul()<<std::endl;
+    std::cout<<b.add()<<std::endl;
 }
